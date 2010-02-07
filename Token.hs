660ddf531e52f41173dee81de65583adae98f998
Francisco Ferreira <fco@FCos-MacBook-Pro.local>
2010-02-07 14:43:56 -0500

    Parser advances

---
+++ Token.hs
@@ -0,0 +1,75 @@
+module Token
+    (
+     HasntToken(..)
+    ,Position
+    )
+    where
+
+
+type Position = (Int, Int) -- (line, col)
+
+data HasntToken 
+     = LineComment		String
+
+-- Special Characters
+
+     | LeftParen	-- (
+     | RightParen	-- )
+     | Comma		-- ,
+     | SemiColon	-- ;
+     | LeftSq		-- [
+     | RightSq		-- ]
+     | BackQuote	-- `
+     | LeftCurly	-- {
+     | RightCurly	-- }
+      
+-- Reserved Words
+
+     | CaseToken
+     | DataToken
+     | DefaultToken
+     | ElseToken
+     | IfToken
+     | InToken
+     | InfixToken
+     | InfixlToken
+     | InfixrToken
+     | LetToken
+     | OfToken
+     | ThenToken
+     | TypeToken
+     | WhereToken
+     | UnusedReservedWord	String
+
+-- Reserved Operands
+
+     | DoubleDotOp	-- ..
+     | ColonOp		-- :
+     | DoubleColonOp	-- ::
+     | EqualsOp		-- =
+     | BackSlashOp	-- \ One back slash
+     | BarOp		-- |
+     | LeftArrowOp	-- <-
+     | RightArrowOp	-- ->
+     | AtOp		-- @
+     | TildeOp		-- ~
+     | DoubleArrowOp	-- =>
+
+-- Variable and Contructor names
+
+     | VariableName 		String 
+     | ConstructorName		String
+     | VariableSymbol 		String 
+
+-- Literals
+
+     | IntegerLiteral 		Int
+     | FloatLiteral 		Double 
+     | StringLiteral 		String 
+     | CharLiteral   		String	-- TODO should change to Char ?
+
+-- CatchAll for errors
+
+     | Unexpected               String
+
+     deriving (Eq, Show)
