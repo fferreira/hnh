9b9b8130a174aa115c2ea1f2e22abaf376fc8562
Francisco Ferreira <fferreira@gmail.com>
2010-02-08 17:01:57 -0500

    LexerHelper.hs cleanup

--- LexerHelper.hs
+++ LexerHelper.hs
@@ -20,20 +20,6 @@ alexGetChar (AlexInput _ []) = Nothing
 alexInputPrevChar :: AlexInput -> Char -- TODO do we need this ?
 alexInputPrevChar _ = error "Lexer doesn't implement alexInputPrevChar"
 
-type Action = Position -> String -> (Position, HasntToken)
-
-{-
-alexScanTokens :: String -> [(Position, HasntToken)]
-alexScanTokens p = scan input
-    where
-      input = AlexInput ("", 1, 1)
-      scan :: AlexInput -> [(position, HasntToken)]
-      scan input = case alexScan input 0 of
-                     AlexError _ -> fail " *** Parser Error ***"
-                     AlexSkip i l -> scan i
-                     AlexToken i l action -> [action] ++ scan i
-  -}                       
-
 -- Internal functions
 
 alexAdvance :: Position -> Char -> Position
