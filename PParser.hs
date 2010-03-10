daa2459b6ad5b270b4454b3ac14b6a73c082d314
Francisco Ferreira <fferreira@gmail.com>
2010-03-10 15:31:58 -0500

    added better whitespace management, still too verbose, but it supports line comments and nested block comments!

--- PParser.hs
+++ PParser.hs
@@ -33,8 +33,20 @@ import Control.Monad(liftM)
 parseHNH :: String -> Either ParseError Program
 parseHNH p = parse program "" p
 
+--- Combinators ---
 
-spcs = between spaces spaces -- adds spaces around a parser
+sepBy2 p sep = do  x1 <- p
+                   x2 <- (sep >> p)
+                   xs <- many (sep >> p)
+                   return (x1:x2:xs)
+
+--- white space and comments ---
+
+whiteSpace = T.whiteSpace haskell
+
+ws = between whiteSpace whiteSpace -- adds spaces around a parser
+
+trailWS = (\p -> do v <- p; whiteSpace ; return v) 
 
 --- Literals ---
 
@@ -50,12 +62,14 @@ stringL :: Parser LiteralValue
 stringL = do char '"'
              x <- many (noneOf "\"") -- TODO add support for escaped chars
              char '"'
+             whiteSpace
              return $ LiteralString x
      
 character :: Parser LiteralValue
 character = do char '\''
                x <- many (noneOf "\'")
                char '\''
+               whiteSpace
                return $ LiteralChar x
 
 
@@ -65,22 +79,24 @@ literal = try float
           <|> try stringL
           <|> try character
           <?> "Literal"
-
+          
 --- Variables and Constructors ---
 
 varid = do f <- many1 lower -- TODO should we avoid keywords?
-           r <- many (alphaNum <|> (char '\''))
+           r <- many (alphaNum <|> (oneOf ",_"))
+--           whiteSpace
            return $ f ++ r
 
 conid = do f <- many1 upper
-           r <- many (alphaNum <|> (char '\''))
+           r <- many (alphaNum <|> (oneOf "'_"))
+           whiteSpace
            return $ f ++ r
            
 symbol = oneOf "!#$%&*+./<=>?@\\^|-~"           
 
-varsym = many1 symbol
+varsym = trailWS $ many1 symbol
 
-variable = parens varsym <|> varid
+variable = trailWS (parens varsym <|> varid)
            
 --- Utils ---           
 
@@ -89,49 +105,54 @@ sqBrackets = between (char '[') (char ']')
 
 --- Program ---
 
-program = do decls <- declarations ; return $ Program decls
+program = do whiteSpace ; decls <- declarations ; whiteSpace ; eof ; return $ Program decls
   
 --- Declarations ---  
 
-declarations = many $ spcs (do decl <- declaration 
-                               optional (spaces >> char ';')
-                               return decl
+declarations = many $ ws (do decl <- declaration 
+                             optional (whiteSpace >> char ';')
+                             whiteSpace
+                             return decl
                            )
 
 declaration = try typeDecl
               <|> try dataDecl
               <|> try typeSigDecl
               <|> fixityDecl
+              <?> "declaration"
 
-typeDecl = do string "type" ; spaces 
-              (n, params) <- simpleType ; spaces
-              char '=' ; spaces
-              t <- typeD ; spaces
+typeDecl = do string "type" ; whiteSpace 
+              (n, params) <- simpleType ; whiteSpace
+              char '=' ; whiteSpace
+              t <- typeD ; whiteSpace
               return $ TypeDcl n params t
               
-dataDecl = do string "data" ; spaces              
-              (n, params) <- simpleType ; spaces
-              char '=' ; spaces
-              cs <- constrs
+dataDecl = do string "data" ; whiteSpace              
+              (n, params) <- simpleType ; whiteSpace
+              char '=' ; whiteSpace
+              cs <- constrs ; whiteSpace
               return $ DataDcl n params cs           
            
-varSpc = do v <- varid; spaces; return v
+varSpc = do v <- varid; whiteSpace; return v
         
 simpleType = do n <- conid
-                spaces
+                whiteSpace
                 params <- many varSpc
+                whiteSpace
                 return (n, params)
                 
-typeSigDecl = do vs <- (spcs variable) `sepBy1` char ','
-                 string "::" ; spaces
+typeSigDecl = do vs <- (ws variable) `sepBy1` char ','
+                 string "::" ; whiteSpace
                  t <- typeD
+                 whiteSpace
                  return $ TypeSigDcl vs t
 
 fixityDecl = do string "infix"                 
                 a <- assocChar
-                spaces
+                whiteSpace
                 p <- (liftM charToInt $ digit) <|>  return 9
-                ops <- (spcs op) `sepBy1` char ','
+                ops <- (ws op) `sepBy1` char ','
+                whiteSpace
                 return $ FixityDcl a p ops 
                   where
                     charToInt :: Char -> Int
@@ -148,48 +169,39 @@ op = do char '`' ; v <- varid ; char '`' ; return v
                 
 constrs = chainr1 constr barSep
 
-barSep = spcs (char '|') >> return (++)
+barSep = ws (char '|') >> return (++)
 
-constr = do n <- conid ; spaces                
-            params <- many aTypeSpc
+constr = do n <- conid ; whiteSpace                
+            params <- many aType
+            whiteSpace
             return $ [ConDcl n params]
             
-aTypeSpc = do spaces ; aType            
-                
-                
 --- Types ---                
 
 aConType = do n <- conid
-              spaces
-              params <- many typeSpc
+              whiteSpace
+              params <- many typeD
               return $ ConType n params
               
-aVarType = do n <- spcs varid ; return $ VarType n              
+aVarType = do n <- ws varid ; return $ VarType n              
 
-typeSpc = do spaces ; t <- typeD; spaces; return t
              
-             
-sepBy2 p sep = do  x1 <- p
-                   x2 <- (sep >> p)
-                   xs <- many (sep >> p)
-                   return (x1:x2:xs)
-             
-aTupleType = try $ parens (do ts <- typeD `sepBy2`(spaces >> char ',' >> spaces)
+aTupleType = try $ parens (do ts <- typeD `sepBy2`(whiteSpace >> char ',' >> whiteSpace)
                               return $ TupleType ts)
              
-aListType = do spaces
-               t <- sqBrackets (do t <- (spcs typeD) ; return $ ConType "List" [t])
-               spaces ; return t
+aListType = do whiteSpace
+               t <- sqBrackets (do t <- (ws typeD) ; return $ ConType "List" [t])
+               whiteSpace ; return t
 
-aType = aConType
-        <|> aVarType
-        <|> aTupleType
-        <|> aListType
-        <|> parens aType
+aType = trailWS (aConType
+                 <|> aVarType
+                 <|> aTupleType
+                 <|> aListType
+                 <|> parens aType)
         
-functionArrow = spcs (string "->") >> return FuncType
+functionArrow = ws (string "->") >> return FuncType
 
-typeD = chainr1 aType functionArrow 
+typeD = trailWS $ chainr1 aType functionArrow 
         
 
 
